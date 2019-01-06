861262c48bd6d2bfde676dc9625e193613bd2e74
Kacper Goleń <golenkacper@gmail.com>
2019-01-06 17:21:06 +0100

    Changed way of starting and moving buttons
    Buttons have been moved down the screen. Changed def build to use sm

--- scratch.py
+++ scratch.py
@@ -2,7 +2,9 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager,Screen, FadeTransition
+from kivy.uix.screenmanager import ScreenManager,Screen, NoTransition
+
+
 
 class MainScreen(Screen):
     pass
@@ -14,11 +16,16 @@ class Lista(Screen):
 class ScreenManagement(ScreenManager):
     pass
 
+Builder.load_file("SimpleKivy.kv")
 
-presentation= Builder.load_file("SimpleKivy.kv")
+sm = ScreenManager(transition=NoTransition())
+sm.add_widget(MainScreen(name='main'))
+sm.add_widget(Mapa(name='mapa'))
+sm.add_widget(Lista(name='lista'))
+
 class MainApp(App):
 
     def build(self):
-        return presentation
+        return sm
 if __name__ == '__main__':
       MainApp().run()
